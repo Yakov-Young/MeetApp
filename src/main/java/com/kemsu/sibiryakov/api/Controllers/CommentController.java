b49a4d2cf3aa2f72a90b0a6824968250eb8163ed
Sibiryakov <suburbanms@gmail.com>
2024-06-16 20:36:05 +0700

    create checkRights, complete Comment part

--- src/main/java/com/kemsu/sibiryakov/api/Controllers/CommentController.java
+++ src/main/java/com/kemsu/sibiryakov/api/Controllers/CommentController.java
@@ -3,6 +3,7 @@ package com.kemsu.sibiryakov.api.Controllers;
 import com.kemsu.sibiryakov.api.DTOs.CreateCommentDTO;
 import com.kemsu.sibiryakov.api.DTOs.BanDTO;
 import com.kemsu.sibiryakov.api.Entities.Emuns.EContentStatus;
+import com.kemsu.sibiryakov.api.Entities.Emuns.ERole;
 import com.kemsu.sibiryakov.api.Entities.MeetPart.Comment;
 import com.kemsu.sibiryakov.api.JwtFilter.JwtFilter;
 import com.kemsu.sibiryakov.api.Services.CommentService;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.kemsu.sibiryakov.api.Services.RightsService.checkRight;
 
 @RestController
 @RequestMapping("/api/comment")
@@ -25,71 +28,93 @@ public class CommentController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Comment> getById(@PathVariable Long id) {
-        Comment comment = commentService.getById(id);
+    public ResponseEntity<Comment> getById(@PathVariable Long id,
+                                           @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER, ERole.MODERATOR, ERole.ADMINISTRATOR,
+                ERole.ORGANIZER, ERole.ADMINISTRATION)) {
+            Comment comment = commentService.getById(id);
 
-        return comment != null
-                ? new ResponseEntity<>(comment, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return comment != null
+                    ? new ResponseEntity<>(comment, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @GetMapping("/getByMeet/active/{id}")
     public ResponseEntity<List<Comment>> getActiveByMeet(@PathVariable("id") Long meetId,
-                                                   @CookieValue("jwt") String jwt) {
-        List<Comment> comments = commentService.getByMeet(meetId);
-        List<Comment> activeComment = new ArrayList<>();
+                                                         @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            List<Comment> comments = commentService.getByMeet(meetId);
+            List<Comment> activeComment = new ArrayList<>();
 
-        for (Comment c : comments) {
-            if (c.getStatus().getStatus().equals(EContentStatus.ACTIVE)) {
-                activeComment.add(c);
+            for (Comment c : comments) {
+                if (c.getStatus().getStatus().equals(EContentStatus.ACTIVE)) {
+                    activeComment.add(c);
+                }
             }
-        }
 
-        return !activeComment.isEmpty()
-                ? new ResponseEntity<>(activeComment, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return !activeComment.isEmpty()
+                    ? new ResponseEntity<>(activeComment, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @GetMapping("/getByMeet/all/{id}")
     public ResponseEntity<List<Comment>> getAllByMeet(@PathVariable("id") Long meetId,
-                                                      @CookieValue("jwt") String jwt) {
-        List<Comment> comments = commentService.getByMeet(meetId);
+                                                      @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            List<Comment> comments = commentService.getByMeet(meetId);
 
-        return !comments.isEmpty()
-                ? new ResponseEntity<>(comments, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return !comments.isEmpty()
+                    ? new ResponseEntity<>(comments, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/ban")
     public ResponseEntity<Comment> banComment(@RequestBody BanDTO banDTO,
-                                              @CookieValue("jwt") String jwt) {
-        Long moderId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                              @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            Long moderId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+                );
 
-        Comment comment = commentService.banComment(banDTO, moderId);
+            Comment comment = commentService.banComment(banDTO, moderId);
 
-        return comment != null
-                ? new ResponseEntity<>(comment, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+            return comment != null
+                    ? new ResponseEntity<>(comment, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Comment> createComment(@RequestBody CreateCommentDTO createCommentDTO,
-                                                 @CookieValue("jwt") String jwt) {
-        Long userId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                                 @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER)) {
+            Long userId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Comment comment = commentService.createComment(createCommentDTO, userId);
+            Comment comment = commentService.createComment(createCommentDTO, userId);
 
-        return comment != null
-                ? new ResponseEntity<>(comment, HttpStatusCode.valueOf(201))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+            return comment != null
+                    ? new ResponseEntity<>(comment, HttpStatusCode.valueOf(201))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
 }
