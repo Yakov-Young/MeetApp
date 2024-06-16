c3aedae9434ad82b7bfb930c41fb9262bddb8c02
Sibiryakov <suburbanms@gmail.com>
2024-06-17 01:34:29 +0700

    setting right MeetControler

--- src/main/java/com/kemsu/sibiryakov/api/Controllers/MeetController.java
+++ src/main/java/com/kemsu/sibiryakov/api/Controllers/MeetController.java
@@ -3,6 +3,7 @@ package com.kemsu.sibiryakov.api.Controllers;
 import com.kemsu.sibiryakov.api.DTOs.MeetDTO.AgreeMeetDTO;
 import com.kemsu.sibiryakov.api.DTOs.BanDTO;
 import com.kemsu.sibiryakov.api.DTOs.MeetDTO.CreateMeetDTO;
+import com.kemsu.sibiryakov.api.Entities.Emuns.ERole;
 import com.kemsu.sibiryakov.api.Entities.MeetPart.Meet;
 import com.kemsu.sibiryakov.api.JwtFilter.JwtFilter;
 import com.kemsu.sibiryakov.api.Services.MeetService;
@@ -13,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.kemsu.sibiryakov.api.Services.RightsService.checkRight;
 
 @RestController
 @RequestMapping("/api/meet")
@@ -26,62 +29,88 @@ public class MeetController {
 
     @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
-    public List<Meet> getAllMeet() {
-        return meetService.getAll();
+    public ResponseEntity<List<Meet>> getAllMeet(@CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.ADMINISTRATOR)) {
+            List<Meet> meets = meetService.getAll();
+
+            return !meets.isEmpty()
+                    ? new ResponseEntity<>(meets, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Meet> getMeetById(@PathVariable Long id) {
-        Meet meet = meetService.getById(id);
-        return meet != null
-                ? new ResponseEntity<>(meet, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+    public ResponseEntity<Meet> getMeetById(@PathVariable Long id,
+                                            @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER, ERole.MODERATOR, ERole.ADMINISTRATOR,
+                ERole.ORGANIZER, ERole.ADMINISTRATION)) {
+            Meet meet = meetService.getById(id);
+            return meet != null
+                    ? new ResponseEntity<>(meet, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Meet> createMeet(@RequestBody CreateMeetDTO createMeetDTO,
-                                           @CookieValue("jwt") String jwt) {
-        Long organizerId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                           @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.ORGANIZER)) {
+            Long organizerId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Meet meet = meetService.createMeet(
-                organizerId,
-                meetService.prepareToCreate(createMeetDTO)
-        );
+            Meet meet = meetService.createMeet(
+                    organizerId,
+                    meetService.prepareToCreate(createMeetDTO)
+            );
 
-        return meet != null
-                ? new ResponseEntity<>(meet, HttpStatusCode.valueOf(201))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+            return meet != null
+                    ? new ResponseEntity<>(meet, HttpStatusCode.valueOf(201))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/agree")
     public ResponseEntity<Meet> approvalMeet(@RequestBody AgreeMeetDTO agreeMeetDTO,
-                                             @CookieValue("jwt") String jwt) {
-        Long administrationId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                             @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.ADMINISTRATION)) {
+            Long administrationId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Meet meet = meetService.approvalMeet(agreeMeetDTO, administrationId);
+            Meet meet = meetService.approvalMeet(agreeMeetDTO, administrationId);
 
-        return new ResponseEntity<>(meet, HttpStatusCode.valueOf(200));
+            return new ResponseEntity<>(meet, HttpStatusCode.valueOf(200));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/ban")
     public ResponseEntity<Meet> banMeet(@RequestBody BanDTO banDTO,
-                                        @CookieValue("jwt") String jwt) {
-        Long administrationId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                        @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.ADMINISTRATION)) {
+            Long administrationId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Meet meet = meetService.banMeet(banDTO, administrationId);
+            Meet meet = meetService.banMeet(banDTO, administrationId);
 
-        return new ResponseEntity<>(meet,HttpStatusCode.valueOf(200));
+            return new ResponseEntity<>(meet, HttpStatusCode.valueOf(200));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 }
