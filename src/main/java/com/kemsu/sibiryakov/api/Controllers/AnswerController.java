917fcc42ebaf89b238ba3720b6bc73c6c21e20f1
Sibiryakov <suburbanms@gmail.com>
2024-06-16 20:55:32 +0700

    setting rights for question and answer, complete content path (without moder panel)

--- src/main/java/com/kemsu/sibiryakov/api/Controllers/AnswerController.java
+++ src/main/java/com/kemsu/sibiryakov/api/Controllers/AnswerController.java
@@ -2,6 +2,7 @@ package com.kemsu.sibiryakov.api.Controllers;
 
 import com.kemsu.sibiryakov.api.DTOs.BanDTO;
 import com.kemsu.sibiryakov.api.DTOs.CreateAnswerDTO;
+import com.kemsu.sibiryakov.api.Entities.Emuns.ERole;
 import com.kemsu.sibiryakov.api.Entities.MeetPart.Answer;
 import com.kemsu.sibiryakov.api.Entities.MeetPart.Question;
 import com.kemsu.sibiryakov.api.JwtFilter.JwtFilter;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.kemsu.sibiryakov.api.Services.RightsService.checkRight;
 
 @RestController
 @RequestMapping("/api/answer")
@@ -22,43 +25,57 @@ public class AnswerController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Answer> getById(@PathVariable Long id) {
-        Answer answer = answerService.getById(id);
+    public ResponseEntity<Answer> getById(@PathVariable Long id,
+                                          @CookieValue(value = "jwt",required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER, ERole.MODERATOR, ERole.ADMINISTRATOR,
+                ERole.ORGANIZER, ERole.ADMINISTRATION)) {
+            Answer answer = answerService.getById(id);
 
-        return answer != null
-                ? new ResponseEntity<>(answer, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return answer != null
+                    ? new ResponseEntity<>(answer, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/ban")
     public ResponseEntity<Answer> banComment(@RequestBody BanDTO banDTO,
-                                             @CookieValue("jwt") String jwt) {
-        Long moderId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                             @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            Long moderId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Answer answer = answerService.banQuestion(banDTO, moderId);
+            Answer answer = answerService.banQuestion(banDTO, moderId);
 
-        return answer != null
-                ? new ResponseEntity<>(answer, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+            return answer != null
+                    ? new ResponseEntity<>(answer, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Answer> createAnswer(@RequestBody CreateAnswerDTO createAnswerDTO,
-                                               @CookieValue("jwt") String jwt) {
-        Long userId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                               @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER)) {
+            Long userId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Answer answer = answerService.createAnswer(createAnswerDTO, userId);
+            Answer answer = answerService.createAnswer(createAnswerDTO, userId);
 
-        return answer != null
-                ? new ResponseEntity<>(answer, HttpStatusCode.valueOf(201))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+            return answer != null
+                    ? new ResponseEntity<>(answer, HttpStatusCode.valueOf(201))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403))
+        }
     }
 }
--- src/main/java/com/kemsu/sibiryakov/api/Controllers/QuestionController.java
+++ src/main/java/com/kemsu/sibiryakov/api/Controllers/QuestionController.java
@@ -3,6 +3,7 @@ package com.kemsu.sibiryakov.api.Controllers;
 import com.kemsu.sibiryakov.api.DTOs.CreateQuestionDTO;
 import com.kemsu.sibiryakov.api.DTOs.BanDTO;
 import com.kemsu.sibiryakov.api.Entities.Emuns.EContentStatus;
+import com.kemsu.sibiryakov.api.Entities.Emuns.ERole;
 import com.kemsu.sibiryakov.api.Entities.MeetPart.Question;
 import com.kemsu.sibiryakov.api.JwtFilter.JwtFilter;
 import com.kemsu.sibiryakov.api.Services.QuestionService;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.kemsu.sibiryakov.api.Services.RightsService.checkRight;
 
 @RestController
 @RequestMapping("/api/question")
@@ -25,71 +28,94 @@ public class QuestionController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Question> getById(@PathVariable Long id) {
-        Question question = questionService.getById(id);
+    public ResponseEntity<Question> getById(@PathVariable Long id,
+                                            @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER, ERole.MODERATOR, ERole.ADMINISTRATOR,
+                ERole.ORGANIZER, ERole.ADMINISTRATION)) {
+            Question question = questionService.getById(id);
 
-        return question != null
-                ? new ResponseEntity<>(question, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return question != null
+                    ? new ResponseEntity<>(question, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @GetMapping("/getByMeet/active/{id}")
     public ResponseEntity<List<Question>> getActiveByMeet(@PathVariable("id") Long meetId,
-                                                    @CookieValue("jwt") String jwt) {
-        List<Question> questions = questionService.getByMeet(meetId);
-        List<Question> activeQuestion = new ArrayList<>();
+                                                          @CookieValue(value = "jwt", required = false) String jwt) {
 
-        for (Question q : questions) {
-            if (q.getStatus().getStatus().equals(EContentStatus.ACTIVE) &&
-                    (q.getAnswer() == null || q.getAnswer().getStatus().getStatus().equals(EContentStatus.ACTIVE))) {
-                activeQuestion.add(q);
+        if (checkRight(jwt, ERole.USER, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            List<Question> questions = questionService.getByMeet(meetId);
+            List<Question> activeQuestion = new ArrayList<>();
+
+            for (Question q : questions) {
+                if (q.getStatus().getStatus().equals(EContentStatus.ACTIVE) &&
+                        (q.getAnswer() == null || q.getAnswer().getStatus().getStatus().equals(EContentStatus.ACTIVE))) {
+                    activeQuestion.add(q);
+                }
             }
-        }
 
-        return !activeQuestion.isEmpty()
-                ? new ResponseEntity<>(activeQuestion, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return !activeQuestion.isEmpty()
+                    ? new ResponseEntity<>(activeQuestion, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @GetMapping("/getByMeet/all/{id}")
     public ResponseEntity<List<Question>> getAllByMeet(@PathVariable("id") Long meetId,
-                                                       @CookieValue("jwt") String jwt) {
-        List<Question> questions = questionService.getByMeet(meetId);
+                                                       @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            List<Question> questions = questionService.getByMeet(meetId);
 
-        return !questions.isEmpty()
-                ? new ResponseEntity<>(questions, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return !questions.isEmpty()
+                    ? new ResponseEntity<>(questions, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/ban")
     public ResponseEntity<Question> banComment(@RequestBody BanDTO banDTO,
-                                               @CookieValue("jwt") String jwt) {
-        Long moderId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                               @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.MODERATOR, ERole.ADMINISTRATOR)) {
+            Long moderId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Question question = questionService.banQuestion(banDTO, moderId);
+            Question question = questionService.banQuestion(banDTO, moderId);
 
-        return question != null
-                ? new ResponseEntity<>(question, HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+            return question != null
+                    ? new ResponseEntity<>(question, HttpStatusCode.valueOf(200))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(404));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Question> createQuestion(@RequestBody CreateQuestionDTO createQuestionDTO,
-                                                   @CookieValue("jwt") String jwt) {
-        Long userId = Long.parseLong(
-                JwtFilter.getBody(jwt)
-                        .get("id")
-                        .toString()
-        );
+                                                   @CookieValue(value = "jwt", required = false) String jwt) {
+        if (checkRight(jwt, ERole.USER)) {
+            Long userId = Long.parseLong(
+                    JwtFilter.getBody(jwt)
+                            .get("id")
+                            .toString()
+            );
 
-        Question question = questionService.createQuestion(createQuestionDTO, userId);
+            Question question = questionService.createQuestion(createQuestionDTO, userId);
 
-        return question != null
-                ? new ResponseEntity<>(question, HttpStatusCode.valueOf(201))
-                : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+            return question != null
+                    ? new ResponseEntity<>(question, HttpStatusCode.valueOf(201))
+                    : new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        } else {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
     }
 }
